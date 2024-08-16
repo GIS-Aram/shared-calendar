cd8e376aadd5312c7062d7e7fbb23bacc6e28dfb
arammousa <aram.mousa@alliander.com>
2024-08-16 13:46:01 +0200

    Backend URL used

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-production-api.com/api'
+  apiUrl: 'https://shared-calendar.onrender.com/api'
+  // apiUrl: 'https://your-production-api.com/api'
   // apiUrl: process.env.API_URL || 'https://fallback-api.com/api'
 };
